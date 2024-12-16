0f49b71caafdafd622593bceb7d46c60e70174cb
CheesyPhoenix <sindre.he.moe@gmail.com>
2024-12-16 19:41:37 +0100

    remove incorrect test

--- src/day11/day11_test.go
+++ src/day11/day11_test.go
@@ -9,9 +9,9 @@ func TestSimple(t *testing.T) {
 	}
 }
 
-func TestSimpleP2(t *testing.T) {
-	res := getResultP2("input_simple.txt")
-	if res != 55312 {
-		t.Fatalf("Expected 55312 but got %d", res)
-	}
-}
+// func TestSimpleP2(t *testing.T) {
+// 	res := getResultP2("input_simple.txt")
+// 	if res != 55312 {
+// 		t.Fatalf("Expected 55312 but got %d", res)
+// 	}
+// }
